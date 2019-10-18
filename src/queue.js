3e1892265cd256bce477fc9e2415bcc976c6caf9
iskanderror <iskanderror@gmail.com>
2019-10-18 12:11:24 +0500

    feat: implement queue

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,34 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+    this.maxSize = 30;
+    if (maxSize!==undefined){
+      this.maxSize = maxSize;
+    }
+    this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+    if (this.heap.size() >= this.maxSize){
+      throw new Error('Heap overflow');
+    }
+    this.heap.push(data,priority);
 	}
 
 	shift() {
-
+    let data = this.heap.pop();
+    if (data === undefined){
+      throw new Error('Heap is empty');
+    }
+    return data;
 	}
 
 	size() {
-
+    return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return (this.size()==0);
 	}
 }
 
